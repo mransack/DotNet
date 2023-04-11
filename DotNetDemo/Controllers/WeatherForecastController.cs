e9d2e6a5c8ffc766ef9eb684e8ea1531aef9251f
Masud <mansack@gmail.com>
2023-04-11 12:22:24 +0530

    added new key

--- DotNetDemo/Controllers/WeatherForecastController.cs
+++ DotNetDemo/Controllers/WeatherForecastController.cs
@@ -6,7 +6,7 @@ namespace DotNetDemo.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries =     
+    private static readonly string[] Summaries = new[]
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
