daa243cdba457e133361683ee93318551b460e4d
Masud <mansack@gmail.com>
2023-06-06 11:42:04 +0530

    Learn - Delegates

---
+++ Learning/Delegates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Learning.DelegateSample;
+
+namespace Learning
+{
+    public class Delegates
+    {
+        //private static WeakReference _weakReference;
+        public Delegates()
+        {
+            Console.WriteLine("Learning Delegates");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("'Delegate' is a type-safe method pointer");
+            Console.WriteLine("'Delegation and actual method declareation should be same");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public void TestMethod()
+        {
+            SumDelegate method = Sum;
+            Console.WriteLine(method(10, 20));
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+    public class DelegateSample
+    {
+        public delegate int SumDelegate(int num1, int num2);
+    }
+}
---
+++ Learning/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Learning;
+
+Console.WriteLine("Welcome to Positivity!");
+
+new Delegates().TestMethod();
